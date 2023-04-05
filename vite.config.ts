5f3904d4d0099a97378de871382e75294fa61c13
Emerson Costa Alves <emerson.esn@gmail.com>
2023-04-04 22:46:54 -0300

    add build file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+      },
+    },
+  },
   plugins: [react()],
-})
+});
